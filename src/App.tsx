2e7ba62ffa378bf78a73b37bbf7d5798a97cf051
sharipovikromjon <ikromjonsharipov924@gmail.com>
2025-06-13 09:33:28 +0500

    initialize React + TypeScript + Vite project with basic setup and configuration

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import QuoteBox from "./components/QuoteBox"
+
+function App() {
+  return (
+    <div><QuoteBox /></div>
+  )
+}
+
+export default App
---
+++ src/components/QuoteBox.tsx
@@ -0,0 +1,10 @@
+// https://api.api-ninjas.com/v1/quotes
+import Button from "@mui/material/Button"
+
+function QuoteBox() {
+  return (
+    <div><Button variant="contained">Contained</Button></div>
+  )
+}
+
+export default QuoteBox
